7ed36f36587e83fd15c4a145e9f08a8c92bd3ff0
SJ-Hyun <89343742+SJ-Hyun@users.noreply.github.com>
2024-07-26 15:06:19 +0900

    Update cron_add.sh

--- cron_add.sh
+++ cron_add.sh
@@ -21,12 +21,14 @@ echo "------------------------"
 
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
 grep -v "$CRON_JOB1" /home/ubuntu/crontab/current_crontab.txt > /home/ubuntu/crontab/new_crontab.txt
- echo "----new crontab ----"
-cat /home/ubuntu/crontab/new_crontab.txt
-echo "------------------------"
+
 
 #  로깅 쉘스크립트 등록 구문을 추가
 echo "$CRON_JOB1" >> /home/ubuntu/crontab/new_crontab.txt
+
+ echo "----new crontab ----"
+cat /home/ubuntu/crontab/new_crontab.txt
+echo "------------------------"
 
 #( 실습용 - 로테이트를 1분에 한번씩 )
 #echo "$CRON_JOB2" >> /home/ubuntu/crontab/new_crontab.txt
@@ -37,7 +39,7 @@ crontab /home/ubuntu/crontab/new_crontab.txt
 
 
 # 임시 파일 삭제
-# rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
+rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
 
 
 # old_log 폴더 없으면 만들어주기 
