ed83d3837c7ccf4eef7c46b0894c987ec3a8e8ea
SJ-Hyun <89343742+SJ-Hyun@users.noreply.github.com>
2024-07-26 15:01:51 +0900

    Update cronjob_add.sh

--- cronjob_add.sh
+++ cronjob_add.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # 크론탭에서 제거할 구문
-CRON_JOB="* * * * * sudo /home/ubuntu/project-log/resource_usage.sh"
+CRON_JOB1="* * * * * sudo /home/ubuntu/project-log/resource_usage.sh"
+CRON_JOB2="* */3 * * * sudo /home/ubuntu/project-log/mail.sh"
 
 # 현재 사용자 크론탭을 임시 파일에 백업
 crontab -l > current_crontab.txt
 
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
-grep -v "$CRON_JOB" current_crontab.txt > new_crontab.txt
+grep -v "$CRON_JOB1" current_crontab.txt > temp_crontab.txt
+grep -v "$CRON_JOB2" temp_crontab.txt > new_crontab.txt
 
 # 새롭게 구문 추가
-echo "$CRON_JOB" >> new_crontab.txt
+echo "$CRON_JOB1" >> new_crontab.txt
+echo "$CRON_JOB2" >> new_crontab.txt
 
 # 새로운 크론탭 파일을 적용
 sudo crontab new_crontab.txt
@@ -18,6 +21,6 @@ sudo crontab new_crontab.txt
 whoami
 
 # 임시 파일 삭제
-sudo rm current_crontab.txt new_crontab.txt
+sudo rm current_crontab.txt temp_crontab.txt new_crontab.txt 
 
-echo "크론탭에서 '$CRON_JOB' 구문을 갱신했습니다."
+echo "크론탭에서 구문을 갱신했습니다."
