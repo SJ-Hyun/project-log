c2f9c3399f5135df8102b7ccc5bf6505003f77ef
SJ-Hyun <89343742+SJ-Hyun@users.noreply.github.com>
2024-07-26 16:45:14 +0900

    Update cronjob_add.sh

--- cronjob_add.sh
+++ cronjob_add.sh
@@ -10,20 +10,23 @@ CRON_JOB2="* */3 * * * sh /home/ubuntu/project-log/mail.sh"
 crontab -l > current_crontab.txt
 
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
-grep -v "* * * * * sh /home/ubuntu/project-log/resource_usage.sh" current_crontab.txt > temp_crontab.txt
-grep -v "* \*\/3 * * * sh /home/ubuntu/project-log/mail.sh" temp_crontab.txt > new_crontab.txt
+sed -i '/$CRON_JOB1/d' current_crontab.txt
+sed -i '/$CRON_JOB2/d' current_crontab.txt
+#grep -v "* * * * * sh /home/ubuntu/project-log/resource_usage.sh" current_crontab.txt > temp_crontab.txt
+#grep -v "* * * * * sh /home/ubuntu/project-log/mail.sh" temp_crontab.txt > new_crontab.txt
 
 # 새롭게 구문 추가
-echo "$CRON_JOB1" >> new_crontab.txt
-echo "$CRON_JOB2" >> new_crontab.txt
+echo "$CRON_JOB1" >> current_crontab.txt
+echo "$CRON_JOB2" >> current_crontab.txt
 
 # 새로운 크론탭 파일을 적용
-crontab new_crontab.txt
+crontab current_crontab.txt
 
 echo "변경 후 크론탭 실행파일입니다."
 crontab -l
 
 # 임시 파일 삭제
-sudo rm current_crontab.txt temp_crontab.txt new_crontab.txt 
+sudo rm current_crontab.txt 
+#temp_crontab.txt new_crontab.txt 
 
 echo "크론탭에서 구문을 갱신했습니다."
