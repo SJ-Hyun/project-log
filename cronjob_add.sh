77759f68e5d2fbd9214da92c6f9e2e001a013909
SJ-Hyun <89343742+SJ-Hyun@users.noreply.github.com>
2024-07-26 16:29:52 +0900

    Update cronjob_add.sh

--- cronjob_add.sh
+++ cronjob_add.sh
@@ -11,17 +11,7 @@ crontab -l > current_crontab.txt
 
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
 grep -v "* * * * * sh /home/ubuntu/project-log/resource_usage.sh" current_crontab.txt > temp_crontab.txt
-echo "-----------------------------"
-echo "current에서 1번 구문 없앤 후"
-echo "*temp_crontab파일*"
-cat temp_crontab.txt
-echo "-----------------------------"
 grep -v "* */3 * * * sh /home/ubuntu/project-log/mail.sh" temp_crontab.txt > new_crontab.txt
-echo "-----------------------------"
-echo "temp에서 2번 구문 없앤 후"
-echo "*new_crontab파일*"
-cat new_crontab.txt
-echo "-----------------------------"
 
 # 새롭게 구문 추가
 echo "$CRON_JOB1" >> new_crontab.txt
