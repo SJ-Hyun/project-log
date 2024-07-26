c289685d5e10cc6df4d7fa2267ebf489bb3c8c1d
SJ-Hyun <89343742+SJ-Hyun@users.noreply.github.com>
2024-07-26 14:47:22 +0900

    Update cron_add.sh

--- cron_add.sh
+++ cron_add.sh
@@ -11,13 +11,19 @@ CRON_JOB1="*/1 * * * * /home/ubuntu/shell/resource_loging.sh"
 #(실습용)
 #CRON_JOB2="*/1 * * * * sudo logrotate -f /etc/logrotate.d/resourcelog"
 
-
+echo "CRON_JOB1 = $CRON_JOB1"
 
 # 현재 돌아가고있는 크론탭을 임시 파일에 백업
 crontab -l > /home/ubuntu/crontab/current_crontab.txt
+echo "----current crontab ----"
+cat /home/ubuntu/crontab/current_crontab.txt
+echo "------------------------"
+
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
 grep -v "$CRON_JOB1" /home/ubuntu/crontab/current_crontab.txt > /home/ubuntu/crontab/new_crontab.txt
- 
+ echo "----new crontab ----"
+cat /home/ubuntu/crontab/new_crontab.txt
+echo "------------------------"
 
 #  로깅 쉘스크립트 등록 구문을 추가
 echo "$CRON_JOB1" >> /home/ubuntu/crontab/new_crontab.txt
@@ -31,7 +37,7 @@ crontab /home/ubuntu/crontab/new_crontab.txt
 
 
 # 임시 파일 삭제
-rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
+# rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
 
 
 # old_log 폴더 없으면 만들어주기 
